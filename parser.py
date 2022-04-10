db167db6f5e41e96c44efab778d388099bfa383e
AndreeaCvl <44116789+AndreeaCvl@users.noreply.github.com>
2022-04-10 21:31:42 +0300

    upd parser

---
+++ parser.py
@@ -0,0 +1,25 @@
+# Parsing toolkit for Python
+from lark import Lark
+
+def test():
+    text = """
+              a = extract [name, age, xx] from image [xyz.jpg]  
+              b = compute mean on a
+              m = compute mean on a[each 12 rows]
+              n = 30 percent [20 to 60] rows a
+              lmplot a
+              save a as csv
+            """
+    run_parser(text)
+
+def run_parser(program):
+    parse_tree = parser.parse(program)
+    print(parse_tree.pretty())
+
+with open("grammar.lark") as grammar:
+    grammar = grammar.read()
+
+parser = Lark(grammar)
+
+if __name__ == '__main__':
+    test()
