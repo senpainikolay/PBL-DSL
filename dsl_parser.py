718c4ccb471156ca5560aef96002af03aa7b720d
AndreeaCvl <44116789+AndreeaCvl@users.noreply.github.com>
2022-04-13 12:21:31 +0300

    upd interpreter

--- dsl_parser.py
+++ dsl_parser.py
@@ -3,6 +3,15 @@ from lark import Lark
 import pandas as pd
 import numpy as np
 
+def children_exists(t):
+    try:
+        t
+    except:
+        var_exists = False
+    else:
+        var_exists = True
+    return var_exists
+
 
 def get_list(t):
     i = 0
@@ -18,41 +27,79 @@ def get_list(t):
 
 def get_df(t):
     key_name = t.children[0]
-    if t.children[1].data == "string_list":
-        lst = get_list(t.children[1])
+    try:
+        if children_exists(t.children[1].data):
+            if t.children[1].data == "string_list":
+                lst = get_list(t.children[1])
+    except:
+        lst = []
 
     data = {'Name': ['Tom', 'Joseph', 'Krish', 'John', 'Maria', 'Ion', 'Andreea'], 'Age': [20, 21, 19, 18, 21, 18, 21]}
     df = pd.DataFrame(data)
 
-    if lst:
+    if len(lst) > 0:
         df = df[lst]
 
     return df
 
 
 def computation(t):
+    if t.children[0].data == "measure":
+        if t.children[0].children[0].children[0] == "mean":
+            df = get_df(t.children[0].children[1])
+            df_mean = 0
 
-    if t.children[0].children[0].children[0] == "mean":
-        df = get_df(t.children[0].children[1])
-        df_mean = 0
-        var_exists = False
-        try:
-            t.children[0].children[2]
-        except:
             var_exists = False
-        else:
-            var_exists = True
-        if var_exists:
-            if t.children[0].children[2].isdigit():
-                each = int(t.children[0].children[2])
-                df_each_x = df.groupby(np.arange(len(df))//each).mean()
-                df_mean = df_each_x
-        else:
-            mean = df.mean()
-            df_mean = pd.DataFrame(mean, columns=['mean'])
-        print(df_mean)
-    return df_mean
+            try:
+                t.children[0].children[2]
+            except:
+                var_exists = False
+            else:
+                var_exists = True
 
+            if var_exists:
+                if t.children[0].children[2].isdigit():
+                    each = int(t.children[0].children[2])
+                    df_each_x = df.groupby(np.arange(len(df))//each).mean()
+                    df_mean = df_each_x
+            else:
+                mean = df.mean()
+                df_mean = pd.DataFrame(mean, columns=['mean'])
+            print(df_mean)
+            return df_mean
+
+    elif t.children[0].data == "percentage":
+        print("percentage")
+        df_percentage = 0
+        df = get_df(t.children[0].children[1])
+
+        # gen only numerical columns
+        cols = np.where(df.dtypes != 'O')
+        df_int = df[df.columns[cols]]
+
+        # number of percents
+        percents = t.children[0].children[0]
+        percents = int(percents) / 100
+
+        try:
+            if children_exists(t.children[0].children[1].data):
+                # if a range is given
+                if t.children[0].children[1].data == "range":
+                    start = t.children[0].children[1].children[0]
+                    end = t.children[0].children[1].children[1]
+                    start = int(start)
+                    end = int(end) + 1
+
+                    df_percentage = df_int.iloc[start:end] * percents
+                    print(df_percentage)
+
+                # no range given
+                elif t.children[0].children[1].data == "df":
+                    df_percentage = df_int * percents
+                    print(df_percentage)
+        except:
+            print("Exception. No range or dataframe given!")
+    return df_percentage
 
 def run_instruction(t):
     if t.data == 'affirmation':
@@ -60,25 +107,25 @@ def run_instruction(t):
 
     elif t.data == 'assignment':
 
-        run_instruction(t.children[0])
         key = t.children[0].children[0]
 
         if t.children[1].data == 'data_extraction':
             print("data extraction")
         elif t.children[1].data == 'computation':
+            print("computation")
             computation(t.children[1])
 
 
 def run_parser(program):
     parse_tree = parser.parse(program)
-    # print(parse_tree)
+    print(parse_tree.pretty())
     for inst in parse_tree.children:
         run_instruction(inst)
     return parse_tree
 
 
 def test():
-    with open("source_code2.txt") as source_code:
+    with open("source_code.txt") as source_code:
         source_code = source_code.read()
     run_parser(source_code)
 
