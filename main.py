4dbf9855be5310158923d8dddc4202512686149d
AndreeaCvl <44116789+AndreeaCvl@users.noreply.github.com>
2022-04-14 18:52:15 +0300

    upd files

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from Parser_DSL import ParserDSL
 from Interpreter import Interpreter
-import warnings
 
 # The grammar of the DSL
 with open("grammar.lark") as grammar:
     grammar = grammar.read()
 
 # The program given for execution
-with open("source_code.txt") as source_code:
+with open("test/source_code.txt") as source_code:
     source_code = source_code.read()
 
 # generating the parse tree
